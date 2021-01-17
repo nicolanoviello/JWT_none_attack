c1ab94498f7d7b86a008c9fbddfc6d071a1116d8
nicolanoviello <nicola.noviello@gmail.com>
2021-01-17 20:44:37 +0100

    Update test_status.py
    Correggo la funzione di test, ora dovrebbe andare

--- test/test_status.py
+++ test/test_status.py
@@ -3,4 +3,4 @@ import requests
 
 def test_status():
     response = requests.get("http://0.0.0.0:5000/status")
-    assert response.content == b'{\n    "message": "Il Sistema non funziona correttamente"\n}\n'
+    assert response.content == b'{\n    "message": "Il Sistema funziona correttamente"\n}\n'
